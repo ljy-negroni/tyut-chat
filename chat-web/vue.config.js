d8e93f7d7c6065741969b30ab2192b2e83ba263e
lijunyii <2336287274@qq.com>
2026-06-15 22:14:13 +0800

    platform 端口 8888 改 8088 避让宝塔，前端 proxy 同步

--- chat-web/vue.config.js
+++ chat-web/vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
 	devServer: {
 		proxy: {
 			'/api': {
-				target: 'http://127.0.0.1:8888',
+				target: 'http://127.0.0.1:8088',
 				changeOrigin: true,
 				ws: false,
 				pathRewrite: {
